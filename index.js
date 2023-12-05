762651dabc6f2dfbf1c06278e7559cf395d72ef9
dangillman <dangillman93@gmail.com>
2023-12-05 14:06:08 -0500

    added content to index.js, completed lab,

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(a,b) {
+    return a + b;
+  }
+  function subtract(a, b) {
+    return a - b;
+  }
+  function multiply(a, b) {
+    return a * b;
+  }
+  function divide(a, b) {
+    return a / b;}
+let number = n
+function increment(n) {
+    return (n + 1)
+}
+function decrement(n) { 
+    return (n - 1)
+}
+function makeInt(string) {
+    const parsedInt = parseInt(string, 10);
+    return parsedInt;
+  }
+function preserveDecimal(string) {
+    const parsedFloat = parseFloat(string);
+    return parsedFloat;
+  }
